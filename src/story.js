d7d8cfe8984df1dc9525d433296295e8a910703b
Alasdair <alasdair123@hotmail.co.uk>
2019-08-07 13:49:34 +0100

    add json object that accomodates options for 3 lines

--- src/story.js
+++ src/story.js
@@ -1,18 +1,34 @@
 import React, { Component } from "react";
+import lines from "./lines.json";
 
 class Story extends Component {
   constructor() {
     super();
     this.state = {
-      storyLineOne: "Once upon a time..."
+      storyLineOne: this.randomNumber1(),
+      storyLineTwo: this.randomNumber2(),
+      storyLineThree: this.randomNumber3()
     };
   }
+
+  randomNumber1 = () => {
+    return Math.floor(Math.random() * lines.line1.length);
+  };
+
+  randomNumber2 = () => {
+    return Math.floor(Math.random() * lines.line2.length);
+  };
+
+  randomNumber3 = () => {
+    return Math.floor(Math.random() * lines.line3.length);
+  };
+
   render() {
     return (
       <div>
         <h2>{this.state.storyLineOne}</h2>
-        <h2>{this.state.storyLineOne}</h2>
-        <h2>{this.state.storyLineOne}</h2>
+        <h2>{this.state.storyLineTwo}</h2>
+        <h2>{this.state.storyLineThree}</h2>
       </div>
     );
   }
