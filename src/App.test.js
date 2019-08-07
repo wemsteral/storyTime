cb341d91f832443e7a83b5febb25640aa74a046d
Alasdair <alasdair123@hotmail.co.uk>
2019-08-07 12:18:25 +0100

    adds first tests with enzyme for App.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { shallow } from "enzyme";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe("First react component test with enzyme", () => {
+  it("renders without crashing", () => {
+    shallow(<App />);
+  });
+
+  it("renders a header", () => {
+    const wrapper = shallow(<App />);
+    const welcome = <h1 className="App-title">StoryTime!</h1>;
+    expect(wrapper.contains(welcome)).toEqual(true);
+  });
 });
